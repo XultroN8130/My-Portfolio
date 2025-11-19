7bdf8481b95bb85707f91e89cb552c6147c5742a
Shantanu Naithani <syntecsys.sn@gmail.com>
2025-11-19 20:26:02 +0530

    Optimized 3D models for mobile loading and performance

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,26 +8,28 @@ import {
   useTexture,
 } from "@react-three/drei";
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from "../Loader"; // correct loader import
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+    <Float speed={1.2} rotationIntensity={1} floatIntensity={1.5}>
+      <ambientLight intensity={0.4} />
+      <directionalLight intensity={0.5} position={[0, 0, 2]} />
+
+      <mesh castShadow receiveShadow scale={2.5}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
+          flatShading
           polygonOffset
           polygonOffsetFactor={-5}
-          flatShading
         />
+        
         <Decal
           position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
+          rotation={[0, 0, 0]}
           scale={1}
           map={decal}
           flatShading
@@ -40,8 +42,8 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
+      frameloop="demand"
+      dpr={[1, 1.5]} // optimized for mobile
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -8,31 +8,42 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive
+      object={earth.scene}
+      scale={2}
+      position={[0, 0, 0]}
+      rotation={[0, 0, 0]}
+    />
   );
 };
 
 const EarthCanvas = () => {
   return (
     <Canvas
-      shadows
-      frameloop='demand'
-      dpr={[1, 2]}
+      frameloop="demand"
+      dpr={[1, 1.2]}   // safer for mobile
       gl={{ preserveDrawingBuffer: true }}
       camera={{
-        fov: 45,
+        fov: 40,
         near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
+        far: 100,
+        position: [-3, 2.5, 5], // slightly closer & lighter load
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
+        
+        {/* Mobile-friendly lighting */}
+        <ambientLight intensity={0.5} />
+        <directionalLight intensity={0.7} position={[5, 5, 5]} />
+
         <OrbitControls
           autoRotate
+          autoRotateSpeed={1.5}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
+
         <Earth />
 
         <Preload all />
